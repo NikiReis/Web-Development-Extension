c4d6926a1302fec8e35259de8b592fd68aa3bd53
LINEK FERREIRA ALMEIDA DOS REIS <linek.reis@a.ucb.br>
2023-08-16 09:34:06 -0300

    correcao

--- atividade1/verificaNumeroPrimo.js
+++ atividade1/verificaNumeroPrimo.js
@@ -4,6 +4,10 @@ function numeroPrimo(num){
             return 0;
         }
     }
-    return 1
+    for(var i = 3;i<num;i+=2){
+        if(num % i === 0){
+            return 1;
+        }
+    }
 }
 console.log(numeroPrimo(8));

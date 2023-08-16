fabddd387a540872644666e0af831c97e72a0935
LINEK FERREIRA ALMEIDA DOS REIS <linek.reis@a.ucb.br>
2023-08-16 10:30:39 -0300

    feat: creating the 4 basic math operations

---
+++ atividade2/variaveisOperadores.js
@@ -0,0 +1,31 @@
+function sum(){
+    const number1 = 6.45;
+    const number2 = 5.63;
+    var result = number1+number2;
+    return `resultado ${result}`;
+}
+
+function division(){
+        const number1 = 3.0;
+        const number2 = 67.0;
+        var result = number1/number2;
+        return `resultado ${result}`;
+    }
+
+function subtraction(){
+    const number1 = 8.65;
+    const number2 = 13.63;
+    var result = number1-number2;
+    return `resultado ${result}`;
+}
+
+function mult(){
+    const number1 = 8.65;
+    const number2 = 3.63;
+    var result = number1*number2;
+    return `resultado ${result}`;
+}
+console.log(mult());
+console.log(sum());
+console.log(subtraction());
+console.log(division());
